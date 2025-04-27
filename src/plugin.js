5bb5b771dc9fca556945b24bb49963bfbb9b75ac
Zyllio <96659995+zyllio@users.noreply.github.com>
2025-04-27 22:59:32 +0200

    -

--- src/plugin.js
+++ src/plugin.js
@@ -1,6 +1,6 @@
 (async function () {
 
-  console.log('SCORM Plugin started 6')
+  console.log('SCORM Plugin started 7')
 
   const ScormMapping = {
     'Lesson location': 'cmi.core.lesson_location',
@@ -40,7 +40,7 @@
 
       const application = zySdk.services.runtime.getApplication()
 
-      const user = zySdk.services.authentication.getCurrentUser()
+      const user = await zySdk.services.authentication.getCurrentUser()
  console.log("user ", user);
 
       const email = user['Email']
