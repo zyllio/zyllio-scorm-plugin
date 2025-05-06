01eec1e91a04a7724fa33a4d096cbace626253d1
Zyllio <96659995+zyllio@users.noreply.github.com>
2025-05-06 15:29:23 +0200

    -

--- src/plugin.js
+++ src/plugin.js
@@ -15,6 +15,7 @@
     'cmi.student_data.time_limit_action': 'Student data time limit action',
     'cmi.suspend_data': 'Suspend data',
     'cmi.comments': 'Comments',
+    'cmi.mode': 'Mode',
 
     // Champs SCORM 2004 réutilisant les libellés existants
     'cmi.location': 'Lesson location',
@@ -129,6 +130,7 @@
         'Session time': newListItem['Session time'] ?? '',
         'Learner name': name,
         'Learner ID': email,
+        'Mode': newListItem['Mode'] ?? '',
         'Date': new Date().toISOString().slice(0, 16).replace('T', ' ')
       }
 
